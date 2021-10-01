6e2e885d82a7ae6f17fc66f3a22d84704acf0986
Raymond W. Ko <raymond.w.ko@gmail.com>
2021-09-30 23:25:30 -0400

    refactoring, convert price to floats

--- src/jvm/app/types/DbKey.java
+++ src/jvm/app/types/DbKey.java
@@ -1,51 +1,42 @@
 package app.types;
 
+import static app.types.Utils.ByteBufferToInstant;
+import static app.types.Utils.ExpDateToInt;
+import static app.types.Utils.InstantToByteBuffer;
+import static app.types.Utils.IntToExpDate;
+import static app.types.Utils.QuoteDateTimeToJodaInstant;
+
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import org.agrona.DirectBuffer;
 import org.agrona.MutableDirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
 import org.apache.commons.collections4.map.LRUMap;
+import org.apache.commons.lang3.StringUtils;
 import org.joda.time.Instant;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
-import org.apache.commons.lang3.StringUtils;
 
 public final class DbKey {
-  private static LRUMap<String, Instant> timestampMsLookupMap = new LRUMap<String, Instant>(1024 * 1024);
-  private static DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
 
   /// use joda time for instant
   public org.joda.time.Instant quoteDateTime;
   /// must be a string of a length of 5
   public String root;
-
-  // P or C
+  /// P or C
   public char optionType;
-
-  // "YYYY-MM-DD"
+  /// "YYYY-MM-DD"
   public String expirationDate;
-
   public int strike;
 
-  private DbKey() {}
-
-  static Instant toJodaInstant(String s) {
-    if (timestampMsLookupMap.containsKey(s)) {
-      return timestampMsLookupMap.get(s);
-    }
-
-    final var t = Instant.parse(s, dateTimeFormatter);
-    timestampMsLookupMap.put(s, t);
-    return t;
-  }
+  public DbKey() {}
 
   public static DbKey fromCsvLineTokens(String[] tokens) {
     DbKey k = new DbKey();
     // [0] is always "^SPX"
 
     // [1] is quote_datetime
-    k.quoteDateTime = toJodaInstant(tokens[1]);
+    k.quoteDateTime = QuoteDateTimeToJodaInstant(tokens[1]);
 
     // [2] is root
     {
@@ -83,46 +74,18 @@ public final class DbKey {
     return k;
   }
 
-  public static int toDateStringInt(String s) {
-    final var year = Integer.parseInt(s.substring(0, 4));
-    final var month = Integer.parseInt(s.substring(5, 7));
-    final var day = Integer.parseInt(s.substring(8, 10));
-    return day + (100 * month) + (10000 * year);
-  }
-
-  public static String toDateString(int i) {
-    final String day = StringUtils.leftPad(String.valueOf(i % 100), 2, "0");
-    i = i / 100;
-    final String month = StringUtils.leftPad(String.valueOf(i % 100), 2, "0");
-    i = i / 100;
-    final String year = String.valueOf(i);
-    return year + "-" + month + "-" + day;
-  }
-  
-  public static ByteBuffer instantToByteBuffer(Instant t) {
-    final long ms = t.getMillis() / 1000;
-    final var buf = ByteBuffer.allocate(8).order(ByteOrder.BIG_ENDIAN).putLong(ms);
-    return buf;
-  }
-  public static Instant byteBufferToInstant(DirectBuffer srcBuf, int i) {
-    final var dstBuf = ByteBuffer.allocate(8).order(ByteOrder.BIG_ENDIAN);
-    srcBuf.getBytes(i, dstBuf, 4, 4);
-    final long ms = dstBuf.getLong(0) * 1000;
-    return new Instant(ms);
-  }
-
   public UnsafeBuffer toBuffer() {
-    final var bb = ByteBuffer.allocateDirect(8 + 5 + 1 + 4 + 4);
+    final var bb = ByteBuffer.allocateDirect(4 + 5 + 1 + 4 + 4);
     final var buf = new UnsafeBuffer(bb);
     var i = 0;
 
-    buf.putBytes(i, instantToByteBuffer(quoteDateTime), 4, 4);
+    buf.putBytes(i, InstantToByteBuffer(quoteDateTime), 4, 4);
     i += 4;
     buf.putStringWithoutLengthUtf8(i, root);
     i += 5;
-    buf.putByte(i, (byte)optionType);
+    buf.putByte(i, (byte) optionType);
     i += 1;
-    buf.putInt(i, toDateStringInt(expirationDate), ByteOrder.BIG_ENDIAN);
+    buf.putInt(i, ExpDateToInt(expirationDate), ByteOrder.BIG_ENDIAN);
     i += 4;
     buf.putInt(i, strike, ByteOrder.BIG_ENDIAN);
     i += 4;
@@ -134,13 +97,13 @@ public final class DbKey {
     final var k = new DbKey();
     var i = 0;
 
-    k.quoteDateTime = byteBufferToInstant(buf, i);
+    k.quoteDateTime = ByteBufferToInstant(buf, i);
     i += 4;
     k.root = buf.getStringWithoutLengthUtf8(i, 5);
     i += 5;
-    k.optionType = (char)buf.getByte(i);
+    k.optionType = (char) buf.getByte(i);
     i += 1;
-    k.expirationDate = toDateString(buf.getInt(i, ByteOrder.BIG_ENDIAN));
+    k.expirationDate = IntToExpDate(buf.getInt(i, ByteOrder.BIG_ENDIAN));
     i += 4;
     k.strike = buf.getInt(i, ByteOrder.BIG_ENDIAN);
     i += 4;
--- src/jvm/app/types/DbValue.java
+++ src/jvm/app/types/DbValue.java
@@ -1,24 +1,27 @@
 package app.types;
 
+import static app.types.Utils.ParsePriceString;
+
+import app.types.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import org.agrona.DirectBuffer;
 import org.agrona.concurrent.UnsafeBuffer;
 
-public final class DbValue {
-  public int open;
-  public int high;
-  public int low;
-  public int close;
+public final class DbValue extends app.types.Buffer {
+  public float open;
+  public float high;
+  public float low;
+  public float close;
   public int trade_volume;
   public int bid_size;
-  public int bid;
+  public float bid;
   public int ask_size;
-  public int ask;
-  public int underlying_bid;
-  public int underlying_ask;
-  public int implied_underlying_price;
-  public int active_underlying_price;
+  public float ask;
+  public float underlying_bid;
+  public float underlying_ask;
+  public float implied_underlying_price;
+  public float active_underlying_price;
   public float implied_volatility;
   public float delta;
   public float gamma;
@@ -26,45 +29,31 @@ public final class DbValue {
   public float vega;
   public float rho;
   public int open_interest;
-  int i;
 
   private DbValue() {
-    this.i = 0;
-  }
-
-  public static int priceStringToInt(String s) {
-    // final var n = s.length();
-    // final char ch = s.charAt(n - 3);
-    // if (ch != '.') {
-    //   throw new RuntimeException("third to last char must be a period");
-    // }
-    // final String a = s.substring(0, n - 3);
-    // final String b = s.substring(n - 2, n);
-    // return Integer.parseInt(a + b);
-    final var f = Float.parseFloat(s) * 100;
-    return (int)Math.floor(f);
+    super();
   }
 
   public static DbValue fromCsvLineTokens(String[] tokens) {
     var v = new DbValue();
     v.i = 0;
 
-    v.open = priceStringToInt(tokens[6]);
-    v.high = priceStringToInt(tokens[7]);
-    v.low = priceStringToInt(tokens[8]);
-    v.close = priceStringToInt(tokens[9]);
+    v.open = ParsePriceString(tokens[6]);
+    v.high = ParsePriceString(tokens[7]);
+    v.low = ParsePriceString(tokens[8]);
+    v.close = ParsePriceString(tokens[9]);
 
     v.trade_volume = Integer.parseInt(tokens[10]);
     v.bid_size = Integer.parseInt(tokens[11]);
-    v.bid = priceStringToInt(tokens[12]);
+    v.bid = ParsePriceString(tokens[12]);
     v.ask_size = Integer.parseInt(tokens[13]);
-    v.ask = priceStringToInt(tokens[14]);
+    v.ask = ParsePriceString(tokens[14]);
 
-    v.underlying_bid = priceStringToInt(tokens[15]);
-    v.underlying_ask = priceStringToInt(tokens[16]);
+    v.underlying_bid = ParsePriceString(tokens[15]);
+    v.underlying_ask = ParsePriceString(tokens[16]);
 
-    v.implied_underlying_price = priceStringToInt(tokens[17]);
-    v.active_underlying_price = priceStringToInt(tokens[18]);
+    v.implied_underlying_price = ParsePriceString(tokens[17]);
+    v.active_underlying_price = ParsePriceString(tokens[18]);
 
     v.implied_volatility = Float.parseFloat(tokens[19]);
     v.delta = Float.parseFloat(tokens[20]);
@@ -78,38 +67,28 @@ public final class DbValue {
     return v;
   }
 
-  private void putInt(UnsafeBuffer buf, int x) {
-    buf.putInt(this.i, x, ByteOrder.BIG_ENDIAN);
-    this.i += 4;
-  }
-
-  private void putFloat(UnsafeBuffer buf, float x) {
-    buf.putFloat(this.i, x, ByteOrder.BIG_ENDIAN);
-    this.i += 4;
-  }
-
   public UnsafeBuffer toBuffer() {
     final var bb = ByteBuffer.allocateDirect(20 * 4);
     final var buf = new UnsafeBuffer(bb);
 
     this.i = 0;
 
-    this.putInt(buf, open);
-    this.putInt(buf, high);
-    this.putInt(buf, low);
-    this.putInt(buf, close);
+    this.putFloat(buf, open);
+    this.putFloat(buf, high);
+    this.putFloat(buf, low);
+    this.putFloat(buf, close);
 
     this.putInt(buf, trade_volume);
     this.putInt(buf, bid_size);
-    this.putInt(buf, bid);
+    this.putFloat(buf, bid);
     this.putInt(buf, ask_size);
-    this.putInt(buf, ask);
+    this.putFloat(buf, ask);
 
-    this.putInt(buf, underlying_bid);
-    this.putInt(buf, underlying_ask);
+    this.putFloat(buf, underlying_bid);
+    this.putFloat(buf, underlying_ask);
 
-    this.putInt(buf, implied_underlying_price);
-    this.putInt(buf, active_underlying_price);
+    this.putFloat(buf, implied_underlying_price);
+    this.putFloat(buf, active_underlying_price);
 
     this.putFloat(buf, implied_volatility);
     this.putFloat(buf, delta);
@@ -123,35 +102,24 @@ public final class DbValue {
     return buf;
   }
 
-  private int getInt(DirectBuffer buf) {
-    final var x = buf.getInt(this.i, ByteOrder.BIG_ENDIAN);
-    this.i += 4;
-    return x;
-  }
-
-  private float getFloat(DirectBuffer buf) {
-    final var x = buf.getFloat(this.i, ByteOrder.BIG_ENDIAN);
-    this.i += 4;
-    return x;
-  }
-
   public static DbValue fromBuffer(DirectBuffer buf) {
     final var v = new DbValue();
     v.i = 0;
 
-    v.open = v.getInt(buf);
-    v.high = v.getInt(buf);
-    v.low = v.getInt(buf);
-    v.close = v.getInt(buf);
+    v.open = v.getFloat(buf);
+    v.high = v.getFloat(buf);
+    v.low = v.getFloat(buf);
+    v.close = v.getFloat(buf);
+
     v.trade_volume = v.getInt(buf);
     v.bid_size = v.getInt(buf);
-    v.bid = v.getInt(buf);
+    v.bid = v.getFloat(buf);
     v.ask_size = v.getInt(buf);
-    v.ask = v.getInt(buf);
-    v.underlying_bid = v.getInt(buf);
-    v.underlying_ask = v.getInt(buf);
-    v.implied_underlying_price = v.getInt(buf);
-    v.active_underlying_price = v.getInt(buf);
+    v.ask = v.getFloat(buf);
+    v.underlying_bid = v.getFloat(buf);
+    v.underlying_ask = v.getFloat(buf);
+    v.implied_underlying_price = v.getFloat(buf);
+    v.active_underlying_price = v.getFloat(buf);
 
     v.implied_volatility = v.getFloat(buf);
     v.delta = v.getFloat(buf);
