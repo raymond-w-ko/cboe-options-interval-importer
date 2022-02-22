76bd41e4057687181cdfd6879fbf646eb4d98248
Raymond W. Ko <raymond.w.ko@gmail.com>
2022-02-22 00:11:10 -0500

    lmdb: cleanup, formatting

--- src/clj/app/lmdb.clj
+++ src/clj/app/lmdb.clj
@@ -1,64 +1,50 @@
 (ns app.lmdb
   (:import
-   [java.time Instant]
-   [java.util.concurrent TimeUnit]
-   [java.nio ByteBuffer ByteOrder]
    [java.nio.channels Channels]
    [org.lmdbjava
-    Env EnvFlags DirectBufferProxy Verifier ByteBufferProxy Txn Stat
-    SeekOp DbiFlags PutFlags]
-   [org.agrona MutableDirectBuffer]
-   [org.agrona.concurrent UnsafeBuffer]
-
-   [app.types DbKey DbValue])
+    Env EnvFlags DirectBufferProxy Verifier ByteBufferProxy Txn Stat SeekOp DbiFlags PutFlags]
+   [org.agrona.concurrent UnsafeBuffer])
   (:require
-   [app.macros :refer [cond-xlet]]
    [clojure.java.io :as io]
-   [clj-java-decompiler.core :refer [decompile]]
    [taoensso.timbre :as timbre
     :refer [log  trace  debug  info  warn  error  fatal  report
-            logf tracef debugf infof warnf errorf fatalf reportf
-            spy get-env]]))
+            logf tracef debugf infof warnf errorf fatalf reportf]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* :warn-on-boxed)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def db-max-size (* 1024 1024 1024 575))
+(def db-max-size (* 1024 1024 1024 1024))
 (def normal-read-env-flags (into-array org.lmdbjava.EnvFlags
                                        [EnvFlags/MDB_RDONLY_ENV]))
-(def dangerous-fast-write-env-flags
-  (into-array org.lmdbjava.EnvFlags
-              [EnvFlags/MDB_FIXEDMAP
-               EnvFlags/MDB_MAPASYNC
-               EnvFlags/MDB_NOMETASYNC
-               EnvFlags/MDB_NOSYNC
-               EnvFlags/MDB_NORDAHEAD]))
+(def dangerous-fast-write-env-flags (into-array org.lmdbjava.EnvFlags
+                                                [EnvFlags/MDB_WRITEMAP
+                                                 EnvFlags/MDB_MAPASYNC
+                                                 EnvFlags/MDB_NOMETASYNC
+                                                 EnvFlags/MDB_NOSYNC
+                                                 EnvFlags/MDB_NORDAHEAD]))
 (defn create-read-env
   (^org.lmdbjava.Env
    [^String db-dir]
    (-> (Env/create DirectBufferProxy/PROXY_DB)
        (.setMapSize db-max-size)
-       (.setMaxDbs 1)
+       (.setMaxDbs 64)
        (.open (io/file db-dir) normal-read-env-flags))))
 (defn  create-write-env
   (^org.lmdbjava.Env
    [^String db-dir]
    (-> (Env/create DirectBufferProxy/PROXY_DB)
        (.setMapSize db-max-size)
-       (.setMaxDbs 1)
+       (.setMaxDbs 64)
        (.open (io/file db-dir) dangerous-fast-write-env-flags))))
 
 (def ^"[Lorg.lmdbjava.DbiFlags;" normal-db-flags
   (into-array org.lmdbjava.DbiFlags [DbiFlags/MDB_CREATE]))
 (defn open-db
-  (^org.lmdbjava.Dbi
-   [^org.lmdbjava.Env env]
+  (^org.lmdbjava.Dbi [^org.lmdbjava.Env env]
    (.openDbi env "main" normal-db-flags))
-  (^org.lmdbjava.Dbi
-   [^org.lmdbjava.Env env
-    ^String db-name]
+  (^org.lmdbjava.Dbi [^org.lmdbjava.Env env ^String db-name]
    (.openDbi env db-name normal-db-flags)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
